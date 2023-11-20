22efa8c12aee9547a3dc4a2d5c455f1c7830a223
MNZSRafael <135064113+MNZSRafael@users.noreply.github.com>
2023-11-20 13:15:27 -0300

    versão final do projeot

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let totalGeral;
+limpar ();
+
 function adicionar ()
 {
 let produto = document.getElementById ("produto").value;
@@ -7,12 +10,20 @@ let quantidade = document.getElementById ("quantidade").value;
 let carrinho = document.getElementById ('lista-produtos');
 let preco = quantidade * valorUnitario;
 
-carrinho.innerHTML = `<section class="carrinho__produtos__produto">
+carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
 <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
 </section>`;
+
+totalGeral = totalGeral + preco;
+let campoTotal = document.getElementById('valor-total');
+campoTotal.textContent = `R$ ${totalGeral}`;
+document.getElementById('quantidade').value = 0;
 
 }
 function limpar()
 {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
 
 }
